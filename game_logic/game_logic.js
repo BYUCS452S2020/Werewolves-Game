8f7b030b059397960754c94d5a5b7d767379a759
jordi1215 <jordi1215.jrc@gmail.com>
2020-05-22 22:35:22 +0000

    The game is functioning with no special characters

--- game_logic/game_logic.js
+++ game_logic/game_logic.js
@@ -1,23 +1,24 @@
 //import stdio from 'stdio';
 
-var round = 0;  // used to store the current round of the game
-var gameover = false;  // boolean value to indicate if the game is over
+let round = 0;  // used to store the current round of the game
+let gameover = false;  // boolean value to indicate if the game is over
 
 const GameManager = require('./game_manager.js')
 let gameManager = new GameManager()
 
-var cmd = require('node-stdio');
+let cmd = require('node-stdio');
 
 
 console.log('Welcome to the village!');
 
 console.log('Please input the number of players(6)');
 
-var playersNum = cmd.readInt();
+let playersNum = cmd.readInt();
 
 gameManager.game_init(playersNum);
 	
 assign_character();
+
 
 while(gameover == false){
 	
@@ -25,17 +26,27 @@ while(gameover == false){
 	
 	gameManager.show_characters();
 	
-	console.log('Let\' go to sleep!');
-	
-	
-	
+	console.log('Let\'s go to sleep!');
+	console.log('Werewolves please kill a player!');
 	kill_character();
 	
+	console.log('Let\'s wake up!');
 	
 	gameover = check_for_game_over();
+	
+	if(gameover == false){
+		console.log('Let\'s vote! ');
+		vote();
+		gameover = check_for_game_over();
+		
+	}
+
+	
 }
 
 gameManager.show_characters();
+
+console.log('Game Over!');
 
 function assign_character(){
 	console.log('assigning characters');
@@ -44,10 +55,10 @@ function assign_character(){
 
 //to kill off a character
 function kill_character(){
-	console.log('Which character do you want to kill?');
-	let characterID = cmd.readInt();
-	gameManager.kill_character(characterID);
-	console.log(`Killing character ${characterID}`);
+	console.log('Which player do you want to kill?');
+	let id = cmd.readInt();
+	gameManager.kill_character(id);
+	console.log(`player ${id} is dead!!`);
 }
 
 
@@ -56,7 +67,37 @@ function debate(){
 }
 
 function vote(){
-	console.log('voting to see who die');
+	gameManager.votes_reset();
+	let winners = [];
+	console.log('Initiating the Voting Process: ');
+	for(let i = 1; i <= gameManager.playerNums; i++){
+		if(gameManager.dict[i].alive){
+			console.log(`Voting for player ${i}`);
+			let vote = cmd.readInt();
+			gameManager.vote_for_character(i, vote);
+			
+		}
+	}
+	winners = gameManager.players_with_max_votes();
+	while(winners.length > 1){
+		gameManager.votes_reset();
+		console.log(`The player\(s\) with the highest vote: ${winners}`);
+		console.log('Looks like we need to vote again!');
+		
+		for(let i = 0 ; i < winners.length; i++){
+			console.log(`Voting for player ${winners[i]}`);
+			let vote = cmd.readInt();
+			gameManager.vote_for_character(winners[i], vote);
+		}
+		winners = gameManager.players_with_max_votes();
+	}
+	
+	
+	console.log(`The player\(s\) with the highest vote: ${winners}`);
+	gameManager.kill_character(winners[0]);
+	console.log(`player ${winners[0]} is dead!!`);
+	
+	
 }
 
 function check_for_game_over(){
--- game_logic/game_manager.js
+++ game_logic/game_manager.js
@@ -19,12 +19,12 @@ class GameManager {
 	
 	// this fuciton is supposed to access the game_type table.Or a new table to get the number of each
 	// characters based on the total number of players
-	// for example, 8 players would equal to 3 villagers, 3 werewolves, 3 special characters. Something like that
+	// for example, 9 players would equal to 3 villagers, 3 werewolves, 3 special characters. Something like that
 	game_init(player_nums = 6){
 		console.log(`initizlazing ${player_nums} players` );
 		this.playerNums = player_nums;
 		var mode = player_nums % 3;
-		var num = player_nums / 3;
+		var num = (player_nums / 3) | 0;
 		
 		if(mode == 0){
 			this.villagers = num;
@@ -32,14 +32,14 @@ class GameManager {
 			this.werewolves = num;
 		}
 		if(mode == 1){
-			this.villagers = num;
+			this.villagers = num + 1;
 			this.specialCharacters = num;
-			this.werewolves = num + 1;
+			this.werewolves = num;
 		}
 		if(mode == 2){
-			this.villagers = num + 1;
+			this.villagers = num;
 			this.specialCharacters = num + 1;
-			this.werewolves = num;
+			this.werewolves = num + 1;
 		}
 		this.characters_init();
 	}
@@ -48,26 +48,24 @@ class GameManager {
 	characters_init(){
 		console.log('initizlazing all the game characters');
 		var id = 1;
+		console.log(`constructing ${this.werewolves} werewolves`);
 		for(var i = 0; i < this.werewolves; i++){
 			let werewolf = new Werewolf(id);
-			this.bad++;
 			this.characterArray.push(werewolf);
 			this.dict[id] = werewolf;
 			id++;
 		}
-		
+		console.log(`constructing ${this.villagers} villagers`);
 		for(var i = 0; i < this.villagers; i++){
 			let villager = new Villager(id);
-			this.good++;
 			this.characterArray.push(villager);
 			this.dict[id] = villager;
 			id++;
 		}
-		
+		console.log(`constructing ${this.specialCharacters} special characters`);
 		for(var i = 0; i < this.specialCharacters; i++){
 			let villager = new Villager(id);
 			this.characterArray.push(villager);
-			this.good++;
 			this.dict[id] = villager;
 			id++;
 		}
@@ -79,10 +77,41 @@ class GameManager {
 		this.dict[character_id].alive = false;
 	}
 	
-	show_characters(){
+	vote_for_character(character_id, votes){
+		this.dict[character_id].vote = votes;
+	}
+	
+	votes_reset(){
 		for(var i = 1; i <= this.playerNums; i++){
-			console.log(`player: ${i}, character: ${this.dict[i].name}, Alive: ${this.dict[i].alive}`)
+			this.dict[i].vote = 0;
 		}
+	}
+	
+	players_with_max_votes(){
+		let maxID = [];
+		let maxVote = -1;
+		for(var i = 1; i <= this.playerNums; i++){
+			if(this.dict[i].vote > maxVote){
+				maxVote = this.dict[i].vote;
+				maxID = [];
+				maxID.push(i);
+			}
+			else if (this.dict[i].vote == maxVote){
+				maxID.push(i);
+			}
+			
+			
+		}
+		return maxID;
+		
+	}
+	
+	show_characters(){
+		console.log('--------------------------------------------------');
+		for(var i = 1; i <= this.playerNums; i++){
+			console.log(`   player: ${i}, character: ${this.dict[i].name}, Alive: ${this.dict[i].alive}`)
+		}
+		console.log('---------------------------------------------------');
 	}
 	
 	game_over(){
@@ -98,7 +127,17 @@ class GameManager {
 				
 			}
 		}
-		return !goodGuys || !badGuys;
+		
+		if(goodGuys == false){
+			console.log('No more villagers left, werewolves win!!');
+			return true;
+		}
+		if(badGuys == false){
+			console.log('No more werewolves left, the villagers win!!');
+			return true;
+		}
+		
+		return false;
 	}
 	
 	
